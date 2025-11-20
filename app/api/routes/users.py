981e2efae1a0d095f10a48797d698d706ce97b7f
¨Paulo¨ <paulofs10.sea@gmail.com>
2025-11-20 16:02:50 -0300

    Improvment/extract-users-route

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter, HTTPException, Query
 from typing import List, Optional
 
-router = APIRouter()
+router = APIRouter(prefix="/users", tags=["users"])
 
+# mock data
+fake_user_db = [
+    {"id": 1, "username": "John_doe", "email": "john@example.com"},
+    {"id": 2, "username": "jane_smith", "email": "jane@example.com"}
+]
+
+@router.get("/")
+async def all_users():
+    """Return all users"""
+    return fake_user_db
+    
 @router.post("/singup")
 async def singup():
     print("singing up")
