fed2f1b4cdb288b907efd74b805847d64426a384
¨Paulo¨ <paulofs10.sea@gmail.com>
2025-12-31 19:01:23 -0300

    feature/proxy-keys

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,19 @@
-from pydantic_settings import BaseSettings
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class Settings(BaseSettings):
+class Settings():
     APP_NAME: str = "YouTube SaaS API"
     DEBUG: bool = os.getenv("DEBUG", False)
     
     # YouTube API Config
     YOUTUBE_API_KEY: str = os.getenv("YOUTUBE_API_KEY", "")
     YOUTUBE_BASE_URL: str = os.getenv("YOUTUBE_BASE_URL", "")
+
+    # Webshare proxy config
+    WEBSHARE_PROXY_USER: str = os.getenv("WEBSHARE_USER",)
+    WEBSHARE_PROXY_PASS: str = os.getenv("WEBSHARE_PASS")
     
     # CORS
     FRONTEND_URL: str = os.getenv("FRONTEND_URL", "")
